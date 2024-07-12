791ec8181051aeb17f0d625b2d7e173c18908677
XING <jackjohnton789@gmail.com>
2024-07-12 10:08:57 +0800

    add project day21

---
+++ day21/script.js
@@ -0,0 +1,39 @@
+const FILL = document.querySelector('.fill');
+const EMPTIES = document.querySelectorAll('.empty');
+
+FILL.addEventListener('dragstart', dragStart);
+FILL.addEventListener('dragend', dragEnd);
+
+for (const empty of EMPTIES) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'fill';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    this.className = 'empty';
+}
+
+function dragDrop() {
+    this.className = 'empty';
+    this.append(FILL);
+}
