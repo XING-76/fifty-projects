c50948ab03dcb5d015279d0bb9d854715d2484a2
XING <jackjohnton789@gmail.com>
2024-07-22 15:55:02 +0800

    add project day29

---
+++ day29/script.js
@@ -0,0 +1,42 @@
+const IMAGE = document.querySelector('.image');
+const TIMES = document.querySelector('#times');
+
+let clickTime = 0;
+let timesClicked = 0;
+
+IMAGE.addEventListener('click', (e) => {
+    if (clickTime === 0) {
+        clickTime = new Date().getTime();
+    } else {
+        if (new Date().getTime() - clickTime < 800) {
+            handleOnTriggerLikeAnimation(e);
+            clickTime = 0;
+        } else {
+            clickTime = new Date().getTime();
+        }
+    }
+});
+
+const handleOnTriggerLikeAnimation = (e) => {
+    const heart = document.createElement('i');
+    heart.classList.add('fas');
+    heart.classList.add('fa-heart');
+
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const leftOffset = e.target.offsetLeft;
+    const topOffset = e.target.offsetTop;
+
+    const xInside = x - leftOffset;
+    const yInside = y - topOffset;
+
+    heart.style.top = `${yInside}px`;
+    heart.style.left = `${xInside}px`;
+
+    IMAGE.appendChild(heart);
+
+    TIMES.innerHTML = ++timesClicked;
+
+    setTimeout(() => heart.remove(), 1000);
+};
